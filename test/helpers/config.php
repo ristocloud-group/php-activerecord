e6a9507f14048626f47a56c11bd0c42c08152682
Vaccari Nicolas <vaccari@ristocloudgroup.com>
2026-07-29 10:35:52 +0200

    Moves the test stack to Compose v2 with MySQL 9.7, MariaDB 11.4, and Postgres 18

--- test/helpers/config.php
+++ test/helpers/config.php
@@ -43,9 +43,10 @@ if (getenv('LOG') !== 'false')
 ActiveRecord\Config::initialize(function(Config $cfg)
 {
 	$cfg->set_connections(array(
-		'mysql'  => getenv('PHPAR_MYSQL')  ?: 'mysql://test:test@127.0.0.1/test',
-		'pgsql'  => getenv('PHPAR_PGSQL')  ?: 'pgsql://test:test@127.0.0.1/test',
-		'sqlite' => getenv('PHPAR_SQLITE') ?: 'sqlite://test.db'));
+		'mysql'   => getenv('PHPAR_MYSQL')   ?: 'mysql://test:test@127.0.0.1/test',
+		'mariadb' => getenv('PHPAR_MARIADB') ?: 'mysql://test:test@127.0.0.1/test',
+		'pgsql'   => getenv('PHPAR_PGSQL')   ?: 'pgsql://test:test@127.0.0.1/test',
+		'sqlite'  => getenv('PHPAR_SQLITE')  ?: 'sqlite://test.db'));
 
 	$cfg->set_default_connection('mysql');
 
